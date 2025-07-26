c7d96f4bb16fc26a7a32331875aaa853383f7b19
Lisa Dean <lisardean82@gmail.com>
2025-07-26 01:34:01 -0400

    test76

--- LisaDTest1.py
+++ LisaDTest1.py
@@ -7,14 +7,21 @@ def hello_world():
     return "Hello!"
 
 def function_1(data):
+    print(type(data), data)
+
     if data == 5:
         return data*3
+    
+
     replacements = {
         2:3,
         10: 55,
         11: 66,
         "twelve": None
         }
+    if isinstance(data, int) and 1 <= data <= 20:
+        return data * (data + 1) // 2
+
         
     return replacements.get(data, data)
 
